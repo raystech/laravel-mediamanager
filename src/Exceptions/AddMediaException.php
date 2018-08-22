640d277c49844822561bece00cb683fe12942d69
Piyapan Rodkuen <piyapannr@gmail.com>
2018-08-23 02:57:36 +0700

    added: media adder exception

---
+++ src/Exceptions/AddMediaException.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Raystech\MediaManager\Exceptions;
+
+use Raystech\MediaManager\Utils\File;
+use Exception;
+
+class AddMediaException extends Exception
+{
+  public static function fileIsTooBig(string $path)
+  {
+    $fileSize = File::getHumanReadableSize(filesize($path));
+
+    $maxFileSize = File::getHumanReadableSize(config('medialibrary.max_file_size'));
+
+    return new static("File `{$path}` has a size of {$fileSize} which is greater than the maximum allowed {$maxFileSize}");
+  }
+
+  public static function fileDoesNotExist(string $path)
+  {
+    return new static("File `{$path}` does not exist");
+  }
+
+  public static function unknownType()
+  {
+    return new static("Only strings, FileObjects and UploadedFileObjects can be imported");
+  }
+}
